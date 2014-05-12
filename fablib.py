03c82064d9cec697b8ecf738243e84b84941f59c
Ryan Nagle <rmnagle@gmail.com>
2014-05-12 10:49:22 -0500

    surround usernames and passwords with double quotes

--- fablib.py
+++ fablib.py
@@ -122,19 +122,19 @@ def verify_prerequisites():
 # Utilities
 def _initial_deploy(dest_path):
     if env.dry_run:
-            ret = local('git ftp init --dry-run --user %s --passwd %s sftp://%s/%s' % (
+            ret = local('git ftp init --dry-run --user "%s" --passwd "%s" sftp://%s/%s' % (
         env.user, env.password, env.host_string, os.path.normpath(dest_path) + os.sep), capture=True)
     else:
-        ret = local('git ftp init --user %s --passwd %s sftp://%s/%s' % (
+        ret = local('git ftp init --user "%s" --passwd "%s" sftp://%s/%s' % (
             env.user, env.password, env.host_string, os.path.normpath(dest_path) + os.sep), capture=True)
     return ret
 
 
 def _deploy(dest_path):
     if env.dry_run:
-        ret = local('git ftp push --dry-run --user %s --passwd %s sftp://%s/%s' % (
+        ret = local('git ftp push --dry-run --user "%s" --passwd "%s" sftp://%s/%s' % (
             env.user, env.password, env.host_string, os.path.normpath(dest_path) + os.sep), capture=True)
     else:
-        ret = local('git ftp push --user %s --passwd %s sftp://%s/%s' % (
+        ret = local('git ftp push --user "%s" --passwd "%s" sftp://%s/%s' % (
             env.user, env.password, env.host_string, os.path.normpath(dest_path) + os.sep), capture=True)
     return ret
