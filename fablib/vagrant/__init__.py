ad53f00eb1dc1329ca73446103b766364e919f69
Ben Keith <ben@benlk.com>
2016-09-15 00:47:17 -0400

    Use correct IP address

--- fablib/vagrant/__init__.py
+++ fablib/vagrant/__init__.py
@@ -6,7 +6,7 @@ from .. import helpers
 __all__ = ['create_db', 'destroy_db', 'load_db', 'dump_db', 'reload_db', 'vagrant', ]
 
 # Local development helpers
-env.vagrant_host = '192.168.33.10'
+env.vagrant_host = '192.168.50.4'
 env.vagrant_db_user = 'root'
 env.vagrant_db_pass = 'root'
 
